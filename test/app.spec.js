1be6c46baf0f8c1f934a4a9787485f24ecc5d13e
Stephanie Bucharelli <stephaniebucharelli@gmail.com>
2020-06-20 22:43:45 -0400

    Found an error in my app.spec.js file - fixed the smallTruck const

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,8 @@
-// asset and describe are mocha commands
+// assert and describe are mocha commands
 const assert = require('assert');
 
 // reference our app code so we can test it
 const app = require('../app.js');
-const { smallTruck } = require('../app.js');
 
 describe('Vehicle Rating Calculator', function () {
     it('calculates a rating of 10 for a vehicle with 4 cylinders and diesel fuel', function () {
@@ -13,7 +12,7 @@ describe('Vehicle Rating Calculator', function () {
     });
 
     it('calculates a rating of 6 for a vehicle with 8 cylinders and regular fuel', function () {
-        const smallCar = app.smallTruck;
+        const smallTruck = app.smallTruck;
         const rating = app.calculateVehicleRating(smallTruck);
         assert.equal(rating, 6);
     });
